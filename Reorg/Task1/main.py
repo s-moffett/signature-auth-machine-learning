a569345969fd45171c3c315d217c9da74aa231fd
Seth Moffett <moffett.seth@gmail.com>
2018-06-17 09:04:49 -0500

    Task 1 - training.

--- Reorg/Task1/main.py
+++ Reorg/Task1/main.py
@@ -12,13 +12,13 @@ import training
 # loading.load()
 
 # Clean data.
-cleaning.clean()
+# cleaning.clean()
 
 # Create features.
-features.create_features()
+# features.create_features()
 
 # Train neural network.
-# training.train_neural_network()
+training.train_neural_network()
 
 # Train logistic regression.
 # training.train_logistic_regression()
