96eb412b95793a3f7061b7bca87f9975dbfb4ba8
s-moffett <s-moffett@users.noreply.github.com>
2018-06-17 15:31:08 -0500

    Graphing

--- Task2/Cleaning/graphs.py
+++ Task2/Cleaning/graphs.py
@@ -8,11 +8,17 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 
 # Plot raw sample.
-# data = pd.read_csv('../../Dataset2/user1.csv')
-data = pd.read_csv('./Data/CSV/Task2/user8.csv')
+# data = pd.read_csv('../../Dataset2/user2.csv')
+data = pd.read_csv('./Reorg/Task1/Data/Input/user2.csv')
 samples = []
 for sig in range(1, 41):
   samples.append(data.loc[data['signature'] == sig])
+
+data2 = pd.read_csv('./Reorg/Task2/Data/Input/user2.csv')
+samples2 = []
+for sig in range(1, 41):
+  samples2.append(data2.loc[data2['signature'] == sig])
+
 
 # plt.plot(samples[0]['time'], samples[0]['x'])
 # plt.plot(samples[0]['time'], samples[0]['y'])
@@ -58,5 +64,9 @@ for sample in range(0, 40):
   plt.plot(samples[sample]['x'], samples[sample]['y'])
 plt.show()
 
-
+# Plot signatures.
+for sample in range(0, 40):
+  plt.subplot(10, 4, sample + 1)
+  plt.plot(samples2[sample]['x'], samples2[sample]['y'])
+plt.show()
 
